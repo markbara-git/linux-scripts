ca73ec40d5d2c7cb8e0743cade8fa314f643e01f
markbara-git <83405406+markbara-git@users.noreply.github.com>
2023-08-16 10:07:32 +0200

    Update fresh-root.sh

--- fresh-root.sh
+++ fresh-root.sh
@@ -3,4 +3,4 @@ apt update
 apt upgrade -y
 apt install sudo -y
 apt autoremove -y
-echo "mbarycki ALL=(ALL) NOPASSWD:ALL\n" > /etc/sudoers.d/mbarycki
+echo "mbarycki ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/mbarycki
